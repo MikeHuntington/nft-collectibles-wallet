7fe0c3b9841b2be72b61dd97c89d40bdd159aa8b
Michael Huntington <2466745+MikeHuntington@users.noreply.github.com>
2021-04-15 14:32:58 -0400

    adding vector icons and redesigning home screen

--- src/navigation/mainNav.js
+++ src/navigation/mainNav.js
@@ -1,14 +1,22 @@
 import React, {useState} from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 
 import {AuthNavigator} from './AuthNavigator'
 import {DrawerNavigator} from './DrawerNavigator'
+
+const baseTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: 'white'
+    },
+  };
 
 const MainNav = () => {
     const [isWalletCreated, setIsWalletCreated] = useState(true)
 
     return (
-        <NavigationContainer>
+        <NavigationContainer theme={baseTheme}>
             {isWalletCreated ? <DrawerNavigator /> : <AuthNavigator />}
         </NavigationContainer>
     )
--- src/screens/main/home.js
+++ src/screens/main/home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 //import {FlatList} from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+import { View } from 'react-native'
 import styled from 'styled-components/native'
 import globalStyles from '../../extra/styles/global'
 import Text from '../../components/Text'
-import { ScrollView } from 'react-native-gesture-handler'
+//import { ScrollView } from 'react-native-gesture-handler'
 
 const items = [
     { name: 'test', key: "1"},
@@ -17,14 +19,21 @@ const items = [
 const Home = ({navigation}) => {
     return (
         <SafeArea>
-            <Navigation>
-                <Text onPress={() => navigation.openDrawer()}>Back</Text>
-            </Navigation>
+            <BackgroundView />
 
-            <Title header>Explore</Title>
-
-            <ScrollView>
+            <ScrollView style={{paddingTop: 50}}>
+                <ScrollHeader>
+                    <ListHeading large color="white">News</ListHeading>
+                    <FlatList 
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={{paddingRight: 20, paddingLeft: 20}}
+                        horizontal 
+                        data={items}
+                        renderItem={({ item }) => (<NewsItem />)}
+                    />
+                </ScrollHeader>
                 <Container>
+                    <Title header>Explore</Title>
                     <ListHeading large heavy>Loot Drops</ListHeading>
                     <FlatList 
                         showsHorizontalScrollIndicator={false}
@@ -35,6 +44,18 @@ const Home = ({navigation}) => {
                     />
                 </Container>
             </ScrollView>
+
+            <Navigation>
+                <Entypo 
+                    onPress={() => navigation.openDrawer()}
+                    name="menu" 
+                    size={24} 
+                    style={{flex: 1}}
+                    color="white" />
+
+                <NavigationTitle large center color="white">Home</NavigationTitle>
+                <View style={{flex: 1}}></View>
+            </Navigation>
         </SafeArea>
     )
 }
@@ -45,27 +66,69 @@ export default Home;
 /* Styles */
 const SafeArea = styled.SafeAreaView`
     flex: 1;
-`;
+`
+
+const ScrollView = styled.ScrollView`
+
+`
 
 const FlatList = styled.FlatList`
     
 `
 
+const BackgroundView = styled.View`
+    background-color: #2F254F;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 40%;
+`
+
+const ScrollHeader = styled.View`
+    padding: 30px 0 20px 0;
+    background-color: #2F254F;
+    height: 190px;
+`
+
 const Navigation = styled.View`
-    padding: 0 32px 32px 32px;
+    position: absolute;
+    padding: 50px 32px 12px 32px;
+    background-color: #2F254F;
+    left: 0;
+    right: 0;
+    flex: 1;
+    flex-direction: row;
+    height: 100px;
+`
+
+const NavigationTitle = styled(Text)`
+    flex: 1;
 `
 
 const Container = styled.View`
-    margin-top: 20px;
+    margin-top: 10px;
+    padding-top: 30px;
+    min-height: 1000px;
+    background-color: white;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
 `;
 
 const FeaturedItem = styled.View`
     border-radius: 30px;
-    background-color: #2F254F;
+    background-color: lightgrey;
     height: 360px;
     width: 320px;
     margin-horizontal: 10px;
+`
 
+const NewsItem = styled.View`
+    border-radius: 40px;
+    background-color: royalblue;
+    height: 80px;
+    width: 200px;
+    margin-horizontal: 10px;
 `
 
 const Title = styled(Text)`
